5ec771b8b1ceee45680af8b75180356c3d35bd75
Fredrik <fueredoriku@gmail.com>
2020-10-14 16:04:48 +0200

    Added rotational movement to towers via UserInput

--- Assets/Scripts/Towers/TowerLogic.cs
+++ Assets/Scripts/Towers/TowerLogic.cs
@@ -6,14 +6,28 @@ public class TowerLogic : MonoBehaviour
 {
     public Vector3 lookingTowards;
     public GameObject bulletSpawnPoint;
+    public GameObject towerRotationPoint;
     //public bool autoShoot;
     public GameObject bullet;
     public float bulletSpeed;
     public float bulletDamage;
+    public UserInput input;
+    public Transform rotAxis;
     // Start is called before the first frame update
     void Start()
     {
         //InvokeRepeating("LaunchProjectile", 2.0f, 1f);
+    }
+    private void FixedUpdate()
+    {
+        changeDirection();
+    }
+    //Movement
+    void changeDirection()
+    {
+        float angle = Mathf.Atan2(input.AimInput.y, input.AimInput.x)*360/Mathf.PI;
+        rotAxis.rotation = Quaternion.Euler(0f, angle, 0f);
+        towerRotationPoint.transform.rotation = rotAxis.rotation;
     }
 
     // Update is called once per frame

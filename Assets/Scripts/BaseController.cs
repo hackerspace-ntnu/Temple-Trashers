dfaad6cd2ea3a9a9c4eb688dcce2f8416104e499
marenksa <55407239+marenksa@users.noreply.github.com>
2020-10-14 23:18:17 +0200

    starter basearbeidet

---
+++ Assets/Scripts/BaseController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    public static BaseController Instance;
+    
+    [SerializeField]
+    private int maxHealth = 101, currentHealth = 101;
+    [SerializeField]
+    private GameObject GUIPrefab;
+
+
+    void Awake(){
+        if(Instance == null){
+            Instance = this;
+        }
+        else{
+            print("Tried to make two bases, removed the latter");
+            Destroy(gameObject);
+        }
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DealDamage(int dmg){
+        currentHealth -= dmg;
+        if(currentHealth <= 0){ Die(); }
+    }
+    private void Die(){
+        //Funksjonalitet for gui og restart
+        //BIG EXPLOTION
+        Instantiate(GUIPrefab);
+    }
+}

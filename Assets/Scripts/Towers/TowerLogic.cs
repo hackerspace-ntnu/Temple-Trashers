53ece807870a6faa263cc89a6fdbcbb0e682aa60
Fredrik <fueredoriku@gmail.com>
2020-10-16 12:22:56 +0200

    Fixed bug related to animation and rotation for tower_wheel

--- Assets/Scripts/Towers/TowerLogic.cs
+++ Assets/Scripts/Towers/TowerLogic.cs
@@ -24,10 +24,10 @@ public class TowerLogic : MonoBehaviour
     }
     //Movement
     void changeDirection()
-    {
+    {/*
         float angle = Mathf.Atan2(input.AimInput.y, input.AimInput.x)*360/Mathf.PI;
         rotAxis.rotation = Quaternion.Euler(0f, angle, 0f);
-        towerRotationPoint.transform.rotation = rotAxis.rotation;
+        towerRotationPoint.transform.rotation = rotAxis.rotation;*/
     }
 
     // Update is called once per frame

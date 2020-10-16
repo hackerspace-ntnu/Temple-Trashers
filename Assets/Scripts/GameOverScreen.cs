c14246b00c6d87afbb3a0d438a93ba48a16e0487
marenksa <55407239+marenksa@users.noreply.github.com>
2020-10-16 14:03:39 +0200

    Spawns GameOverScreen UI on base death with working restart-button

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
     void Start()
     {
-        Debug.Log("GAME OVER");
+        Debug.Log("Game Over Screen Instantiated");
+    }
+
+    public void Restart()
+    {
+        Debug.Log("RESTARTED");
+        // Reloads the scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }

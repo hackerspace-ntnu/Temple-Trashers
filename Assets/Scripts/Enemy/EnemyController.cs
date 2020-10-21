6ce55fa28cc594bbd83cafec7e4012e978b95af9
Dabble <ddabble@users.noreply.github.com>
2020-10-21 16:53:34 +0200

    Made enemy use navmesh

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,43 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
     public Transform playerBase;
-    public float movementSpeed = 5;
 
-    private new Rigidbody rigidbody;
+    private NavMeshAgent agent;
 
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
-    }
-
-    void Update()
-    {
-        RotateTowardsTarget(playerBase);
+        agent = GetComponent<NavMeshAgent>();
     }
 
     void FixedUpdate()
     {
-        Vector3 directionToBase = (playerBase.position - transform.position).normalized;
-        MoveTowardsTarget(directionToBase);
-    }
-
-    private void MoveTowardsTarget(Vector3 direction)
-    {
-        Vector3 targetVelocity = movementSpeed * direction;
-        Vector3 targetVelocityDiff = targetVelocity - rigidbody.velocity;
-        // Prevent movement along the y axis
-        targetVelocityDiff.y = 0;
-        rigidbody.AddForce(targetVelocityDiff, ForceMode.Impulse);
-    }
-
-    private void RotateTowardsTarget(Transform target)
-    {
-        transform.LookAt(target);
-        // Set x and y rotation to 0, so that the transform is only rotated around the y axis
-        transform.eulerAngles = transform.eulerAngles.SetOnly(x: 0, z: 0);
+        agent.destination = playerBase.position;
     }
 }

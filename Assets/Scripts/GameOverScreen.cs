3d82e3ae4d4a4cdced7339d82b81ebd2c089449f
Livia Emilie Valenti Stokke <lestokke@stud.ntnu.no>
2020-10-27 18:53:31 +0100

    Add: Countdown on game over screen

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -1,15 +1,36 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverScreen : MonoBehaviour
 {
+    [SerializeField]
+    private Text timerText;
     void Start()
     {
         Debug.Log("Game Over Screen Instantiated");
+        StartCoroutine(Countdown(5));
     }
 
-    public void Restart()
+
+
+    IEnumerator Countdown(int seconds)
     {
+        int counter = seconds;
+        while (counter > 0)
+        {
+            timerText.text = counter.ToString();
+            yield return new WaitForSeconds(1);
+            // Fix UI
+            counter--;
+        }
+        Restart();
+    }
+
+
+    public void Restart()
+    {   
         Debug.Log("RESTARTED");
         // Reloads the scene
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);

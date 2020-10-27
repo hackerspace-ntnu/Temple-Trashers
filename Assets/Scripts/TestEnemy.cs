1cc95fb838b642dd1d1186726a83a52e9e307863
Livia Emilie Valenti Stokke <lestokke@stud.ntnu.no>
2020-10-27 17:54:39 +0100

    Add: Test enemy script

---
+++ Assets/Scripts/TestEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEnemy : MonoBehaviour
+{
+    int dmg = 200;
+
+    void OnCollisionEnter()
+    {
+        Debug.Log("HIT");
+        BaseController.Instance.DealDamage(dmg);
+    }
+}

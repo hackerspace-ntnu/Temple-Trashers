5ff50b08245e891af1b847267b5d5995e3cf85bd
Fredrik <fueredoriku@gmail.com>
2020-10-28 16:11:41 +0100

    Added an animation to lightning_tower with an event that cools a function that is going to shoot lightning

---
+++ Assets/shootLightning.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootLightning : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void shoot()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

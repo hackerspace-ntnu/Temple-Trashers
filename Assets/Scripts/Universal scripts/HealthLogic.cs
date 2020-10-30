29740689329ea347175019640f55c0aff6454f2a
Dabble <ddabble@users.noreply.github.com>
2020-10-30 14:48:48 +0100

    Fixed missing renaming of methods

--- Assets/Scripts/Universal scripts/HealthLogic.cs
+++ Assets/Scripts/Universal scripts/HealthLogic.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class HealthLogic : MonoBehaviour
 {
     public delegate void onStatusUpdate();
+
     public onStatusUpdate OnDeath;
 
     public float health;
     public float maxHealth;
-    
-    public virtual void dealDamage(float input)
+
+    public virtual void DealDamage(float input)
     {
         if (health <= input)
         {
@@ -21,9 +22,10 @@ public class HealthLogic : MonoBehaviour
         {
             health -= input;
         }
-
     }
-    public virtual void Heal(float input) {
+
+    public virtual void Heal(float input)
+    {
         health += input;
         if (health > maxHealth && maxHealth > 0)
         {

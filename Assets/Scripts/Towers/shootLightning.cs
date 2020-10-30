adc856c3e093c0773150a7f19ac06c59989638f3
Fredrik <fueredoriku@gmail.com>
2020-10-30 02:19:47 +0100

    Added a functioning shooting script for the lightningtower. damage will chain through every gameobject using healthscript within radius of eachother. Also fixed animation and model for the lightningtower.

---
+++ Assets/Scripts/Towers/shootLightning.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootLightning : MonoBehaviour
+{
+    public float LightningRadius;
+    public float damage;
+    //public int maxTargets;
+    private List<GameObject> ZappTargets = new List<GameObject>();
+    public void shoot()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, LightningRadius);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.GetComponent<HealthLogic>())
+            {
+                addZap(hitCollider.gameObject);
+                checkZap(hitCollider.gameObject);
+            }
+        }
+        foreach (var zap in ZappTargets)
+        {
+            zap.GetComponent<HealthLogic>().dealDamage(damage);
+        }
+        ZappTargets.Clear();
+    }
+
+
+
+
+    private void addZap(GameObject target)
+    {
+        if (target.GetComponent<HealthLogic>() && !ZappTargets.Contains(target))
+        {
+            ZappTargets.Add(target);
+        }
+    }
+
+    private void checkZap(GameObject target)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(target.transform.position, LightningRadius);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (!ZappTargets.Contains(hitCollider.gameObject) && hitCollider.GetComponent<HealthLogic>())
+            {
+                addZap(hitCollider.gameObject);
+                checkZap(hitCollider.gameObject);
+            }
+        }
+    }
+}

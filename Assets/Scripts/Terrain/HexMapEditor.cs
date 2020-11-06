8bb97f9ad00a49dccab37c07080752969432a9f8
Thomas Jakob Gabrielsen <thomajga@ntnu.no>
2020-11-06 15:37:56 +0100

    Terrain is now divided into chunks

--- Assets/Scripts/Terrain/HexMapEditor.cs
+++ Assets/Scripts/Terrain/HexMapEditor.cs
@@ -36,7 +36,6 @@ public class HexMapEditor : MonoBehaviour {
     {
 		cell.color = activeColor;
 		cell.Elevation = activeElevation;
-		hexGrid.Refresh();
     }
 
 	public void SetElevation (float elevation)

718441bd1c800a017c851b8eac351856a703c31d
Fredrik <fueredoriku@gmail.com>
2021-01-21 18:01:51 +0100

    Added a basic structure for making different selction-based UIs from scriptableObjects.

---
+++ Assets/Scripts/UI/FlexibleUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlexibleUI : MonoBehaviour
+{
+    public FlexibleUIData skinData;
+
+
+
+    //Let's you edit UI and update the corresponding scriptoableobject automaticly
+    protected virtual void onSkinUI()
+    {
+
+    }
+    public virtual void Awake()
+    {
+        onSkinUI();
+    }
+
+    public virtual void Update()
+    {
+        if (Application.isEditor)
+        {
+            onSkinUI();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/FlexibleUIButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(Image))]
+public class FlexibleUIButton : FlexibleUI
+{
+    
+}

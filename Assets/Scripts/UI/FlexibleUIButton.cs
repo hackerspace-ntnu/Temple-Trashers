b88419722a21ac88acd3e97e5c3bb1f2bb30b205
Fredrik <fueredoriku@gmail.com>
2021-02-04 18:04:38 +0100

    Added a very basic default skin for UI-elements, and added a very basic radial menu implementing said default skin

--- Assets/Scripts/UI/FlexibleUIButton.cs
+++ Assets/Scripts/UI/FlexibleUIButton.cs
@@ -7,5 +7,26 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Image))]
 public class FlexibleUIButton : FlexibleUI
 {
-    
+    Image image;
+    Button button;
+
+    protected override void onSkinUI()
+    {
+        base.onSkinUI();
+        image = GetComponent<Image>();
+        button = GetComponent<Button>();
+
+        button.transition = Selectable.Transition.SpriteSwap;
+        button.targetGraphic = image;
+
+        //Setting the UI-objects to match the scriptableObject:
+        image.sprite = skinData.buttonSprite;
+        image.type = Image.Type.Sliced;
+        button.spriteState = skinData.buttonSpriteState;
+        
+      
+
+
+
+    }
 }

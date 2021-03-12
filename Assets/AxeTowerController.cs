0c8acc76e76c6ab0ae046a8a53f8985316c57d1e
Sondre Husøy <sondre@primecut.no>
2021-03-12 15:48:33 +0100

    Added model and soome functionality for axe turret

---
+++ Assets/AxeTowerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeTowerController : MonoBehaviour
+{
+    public MeshRenderer handAxe, totemAxe;
+    public Transform spawnPoint;
+    public GameObject axeProjectilePrefab;
+    public float projectileSpeed = 5f;
+    public Transform forwardTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void grab()
+    {
+        handAxe.enabled = true;
+        totemAxe.enabled = false;
+    }
+    public void toss()
+    {
+        handAxe.enabled = false;
+        Rigidbody projectileBody =  Instantiate(axeProjectilePrefab, spawnPoint.position, spawnPoint.rotation).GetComponent<Rigidbody>();
+        projectileBody.velocity = forwardTransform.right * projectileSpeed;
+    }
+    public void spawn()
+    {
+        totemAxe.enabled = true;
+    }
+}

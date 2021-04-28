2124fb6ab8d090413780ff7dfcb06354cfb16395
Fredrik <fredrlei@stud.ntnu.no>
2021-04-29 00:04:15 +0200

    Created an inventoryManager as a singleton.

---
+++ Assets/Scripts/Util/InventoryManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    private static InventoryManager _instance;
+
+    public static InventoryManager Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+}

223ecdefbf1bc87b99ddde870a996825a535bcc4
Fredrik <fredrlei@stud.ntnu.no>
2021-04-29 00:29:10 +0200

    Added logic to modify number of resources collected.

--- Assets/Scripts/Util/InventoryManager.cs
+++ Assets/Scripts/Util/InventoryManager.cs
@@ -4,11 +4,57 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    //Singleton related logic
     private static InventoryManager _instance;
-
     public static InventoryManager Instance { get { return _instance; } }
 
+    //Total amount of resources.
+    private int resourceAmount = 0;
 
+
+    //Get-er
+    public int GetResourceAmount()
+    {
+        return resourceAmount;
+    }
+
+    //Adders
+    public void AddResource(int amount)
+    {
+        if (amount > 0)
+        {
+            resourceAmount += amount;
+        }
+    }
+    public void AddResource()
+    {
+        resourceAmount ++;
+    }
+
+    //Subtracters
+    public bool SubtractResource(int amount)
+    {
+        if (amount <= resourceAmount) {
+            resourceAmount -= amount;
+            return true;
+        }
+        return false;
+    }
+    public bool SubtractResource()
+    {
+        if (resourceAmount >= 1)
+        {
+            resourceAmount --;
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+
+    //Singleton-related function
     private void Awake()
     {
         if (_instance != null && _instance != this)

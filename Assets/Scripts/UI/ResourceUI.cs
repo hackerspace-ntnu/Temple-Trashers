508059d7150c389f4bd5139263e70372164d5839
Fredrik <fredrlei@stud.ntnu.no>
2021-04-30 01:41:20 +0200

    Added a basic UI to show amount of resources.

--- Assets/Scripts/UI/ResourceUI.cs
+++ Assets/Scripts/UI/ResourceUI.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResourceUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text resourceAmount = null;
+
+    private InventoryManager inventory;
+
     void Start()
     {
-        
+        inventory = InventoryManager.Instance;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        resourceAmount.text = inventory.GetResourceAmount().ToString();
     }
 }

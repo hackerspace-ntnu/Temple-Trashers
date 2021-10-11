acdf9ba7199df836f46124604d1385df3bcf0d35
Dabble <ddabble@users.noreply.github.com>
2021-10-11 18:43:00 +0200

    Removed unused code in TurretInput

--- Assets/Scripts/Towers/TurretInput.cs
+++ Assets/Scripts/Towers/TurretInput.cs
@@ -4,44 +4,15 @@ using UnityEngine;
 
 public class TurretInput : MonoBehaviour
 {
-    public GameObject tower;
-
-    private IEnumerator coroutine;
     private PlayerStateController player;
-    private HexGrid hexGrid;
 
     void Start()
     {
-        hexGrid = GameObject.Find("Terrain").GetComponent<HexGrid>();
         player = GetComponent<PlayerStateController>();
-
-        coroutine = WaitAndPrint(5.0f);
-        StartCoroutine(coroutine);
-    }
-
-    private IEnumerator WaitAndPrint(float waitTime)
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(waitTime);
-            //PlaceTower(tower, transform.position);
-        }
     }
 
     public Vector2 GetAimInput()
     {
         return player.AimInput;
-    }
-
-    public PlayerStates CurrentPlayerState()
-    {
-        return player.CurrentState;
-    }
-
-    public void PlaceTower(GameObject tower, Vector3 position)
-    {
-        HexCell cell = hexGrid.GetCell(position);
-        cell.isOccupied = true;
-        cell.occupier = Instantiate(tower, cell.transform);
     }
 }

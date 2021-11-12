cb452e7a3e941413bb42ef87d66275dca1d6ae74
Fredrik Lillemoen Eiding <fredrlei@stud.ntnu.no>
2021-11-13 00:31:46 +0100

    Added the ability to pause, and a menu that is instantiated with option to quit game.

---
+++ Assets/Scripts/GameState/PauseManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField]
+    private static GameObject ui;
+
+    public static PauseManager Singleton { get; private set; }
+    
+
+    private static bool pauseStatus = false;
+   
+    public static bool isPaused()
+    {
+        return pauseStatus;
+    }
+
+    public static void pauseGame()
+    {
+        if (pauseStatus == false)
+        {
+            pauseStatus = true;
+            Time.timeScale = 0;
+            ui.SetActive(pauseStatus);
+        }
+        else
+        {
+            pauseStatus = false;
+            Time.timeScale = 1;
+            ui.SetActive(pauseStatus);
+        }
+    }
+
+    public static void quitGame()
+    {
+        Application.Quit();
+    }
+
+    void Awake()
+    {
+        
+        #region Singleton boilerplate
+
+        if (Singleton != null)
+        {
+            if (Singleton != this)
+            {
+                Debug.LogWarning($"There's more than one {Singleton.GetType()} in the scene!");
+                Destroy(gameObject);
+            }
+
+            return;
+        }
+
+        Singleton = this;
+
+        #endregion Singleton boilerplate
+
+        ui = GameObject.Find("PauseMenu");
+        if (ui == null)
+        {
+            Debug.Log("missing Pause UI in scene.");
+        }
+        else
+        {
+            ui.SetActive(pauseStatus);
+        }
+    }
+}

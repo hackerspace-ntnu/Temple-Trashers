49bbf1f764121d2db0c20a29b1e47baf7ac9781e
Fredrik Eiding <fueredoriku@gmail.com>
2022-02-12 23:18:44 +0100

    Added basic functionality for repairmechanics and states.

---
+++ Assets/Scripts/Towers/RepairController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairController : MonoBehaviour
+{
+    [SerializeField]
+    private float damageInterval;
+
+    [SerializeField]
+    private HealthLogic healthLogic;
+
+    [SerializeField]
+    private int durability;
+
+    private int wearDamage;
+
+    private float timeStamp;
+    private bool needRepair = false;
+
+    public void Repair(){
+        needRepair = false;
+    }
+
+    void Start()
+    {
+        timeStamp = Time.deltaTime;    
+    }
+
+    void Update()
+    {
+        if (needRepair) { healthLogic.DealDamage(wearDamage); }
+        if (timeStamp < Time.deltaTime + damageInterval){
+            timeStamp = Time.deltaTime;
+            needRepair = true;
+        }
+    }
+}

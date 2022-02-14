4aa778d59bfaf1ad5d9790cc7e077e032c66a03b
Fredrik Eiding <fueredoriku@gmail.com>
2022-02-14 11:11:41 +0100

    Placed files in a better place in hierarchy.

---
+++ Assets/Scripts/Towers/Repair logic/RepairAnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairAnimationController : MonoBehaviour
+{
+    public void changeStateNone()
+    {
+
+    }
+    public void changeStateLow()
+    {
+
+    }
+    public void changeStateMedium()
+    {
+
+    }
+    public void changeStateHigh()
+    {
+
+    }
+}

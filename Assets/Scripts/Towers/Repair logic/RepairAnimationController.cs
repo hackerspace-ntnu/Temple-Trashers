7859ed616d8ee9ba8e6c9420732916c2c46feb15
Fredrik Eiding <fueredoriku@gmail.com>
2022-02-15 01:15:13 +0100

    Added wrappers for each repair-related VFX. Updated RepairAnimationController to instantiate particles based on state. The script should subscribe to RepairController delegate, but that is WIP.

--- Assets/Scripts/Towers/Repair logic/RepairAnimationController.cs
+++ Assets/Scripts/Towers/Repair logic/RepairAnimationController.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class RepairAnimationController : MonoBehaviour
 {
-    public void changeStateNone()
-    {
+    //Class references
+    public GameObject fxLow;
+    public GameObject fxMedium;
+    public GameObject fxHigh;
 
+    //Instances
+    private GameObject _fxLow;
+    private GameObject _fxMedium;
+    private GameObject _fxHigh;
+
+    public void DeleteInstances()
+    {
+        if (_fxLow) { Destroy(_fxLow); }
+        if (_fxMedium) { Destroy(_fxMedium); }
+        if (_fxHigh) { Destroy(_fxHigh); }
     }
-    public void changeStateLow()
-    {
 
-    }
-    public void changeStateMedium()
+    public void UpdateParticles(WearState newState, WearState previousState)
     {
-
-    }
-    public void changeStateHigh()
-    {
-
+       switch (newState)
+        {
+            case WearState.NONE:
+                DeleteInstances();
+                break;
+            case WearState.LOW:
+                _fxLow = Instantiate(fxLow);
+                break;
+            case WearState.MEDIUM:
+                _fxMedium = Instantiate(fxMedium);
+                break;
+            case WearState.HIGH:
+                _fxHigh = Instantiate(fxHigh);
+                break;
+            default:
+                DeleteInstances();
+                break;
+        }
     }
 }

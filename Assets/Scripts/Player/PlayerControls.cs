e22576a71eda258f254485402c290f8591372a09
Thomas Jakob Gabrielsen <thomajga@stud.ntnu.no>
2022-02-26 21:57:30 +0100

    Updated and animated the new main menu

--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Prefabs/PlayerControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/InputSettings/PlayerControls.inputactions'
 
 using System;
 using System.Collections;

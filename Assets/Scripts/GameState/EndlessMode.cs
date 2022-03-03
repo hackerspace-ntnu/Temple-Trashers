7e404f809eaef558b3d12b6f65da9d41a37702c8
Dabble <ddabble@users.noreply.github.com>
2022-03-03 22:29:51 +0100

    Added probability weighting to enemy spawning
    Also made `EndlessModeManager` spawn unigolems (with a 1/10 probability).

--- Assets/Scripts/GameState/EndlessMode.cs
+++ Assets/Scripts/GameState/EndlessMode.cs
@@ -1,5 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+
+[Serializable]
+public struct EnemySpawnData
+{
+    public Enemy prefab;
+    public int probabilityWeight;
+}
 
 
 public class EndlessMode : MonoBehaviour
@@ -8,7 +19,7 @@ public class EndlessMode : MonoBehaviour
 
     // Serialized variables
     [SerializeField]
-    private Enemy[] enemyPrefabs;
+    private EnemySpawnData[] enemyPrefabs;
 
     [Header("Enemies: f(x) = linearSpawnRate * x^(exponentialSpawnRate)")]
     [Range(1, 10), SerializeField]
@@ -23,8 +34,10 @@ public class EndlessMode : MonoBehaviour
 
     // Private variables
     private int waveNumber = 1;
-
     private float timeSinceLastWaveStart = 0;
+
+    private (int, int, Enemy)[] spawnProbabilityRangesAndEnemies;
+    private int spawnProbabilityWeightSum;
 
     void Awake()
     {
@@ -44,6 +57,23 @@ public class EndlessMode : MonoBehaviour
         Singleton = this;
 
         #endregion Singleton boilerplate
+
+        spawnProbabilityRangesAndEnemies = new (int, int, Enemy)[enemyPrefabs.Length];
+        int cumulativeWeight = 0;
+        // Fill the `spawnProbabilityRangesAndEnemies` array with tuples each covering a sub-range
+        // of the entire probability range from 0 to `spawnProbabilityWeightSum`
+        for (int i = 0; i < enemyPrefabs.Length; i++)
+        {
+            EnemySpawnData spawnData = enemyPrefabs[i];
+            int probabilityRangeStart = cumulativeWeight;
+            int probabilityRangeEnd = cumulativeWeight + spawnData.probabilityWeight;
+            spawnProbabilityRangesAndEnemies[i] = (
+                probabilityRangeStart, probabilityRangeEnd, spawnData.prefab
+            );
+            cumulativeWeight += spawnData.probabilityWeight;
+        }
+
+        spawnProbabilityWeightSum = cumulativeWeight;
     }
 
     void Start()
@@ -80,9 +110,37 @@ public class EndlessMode : MonoBehaviour
     /// </summary>
     private void SpawnEnemy()
     {
-        GameObject prefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)].gameObject;
+        Enemy enemyToSpawn = ChooseRandomEnemy();
         HexCell[] edgeCells = HexGrid.Singleton.edgeCells;
         Vector3 spawnPos = edgeCells[Random.Range(0, edgeCells.Length - 1)].transform.position;
-        Instantiate(prefab, spawnPos, Quaternion.identity, transform);
+        Instantiate(enemyToSpawn.gameObject, spawnPos, Quaternion.identity, transform);
+    }
+
+    private Enemy ChooseRandomEnemy()
+    {
+        int spawnProbabilityPoint = Random.Range(0, spawnProbabilityWeightSum);
+        // The probability range used for searching should only be a single point
+        (int, int, Enemy) searchTuple = (spawnProbabilityPoint, spawnProbabilityPoint, null); // the enemy doesn't affect the search, and so can be `null`
+        int foundIndex = Array.BinarySearch(spawnProbabilityRangesAndEnemies,
+            searchTuple,
+            Comparer<(int, int, Enemy)>.Create(
+                // `rangeB` is always the search point (`searchTuple`)
+                (rangeA, rangeB) =>
+                {
+                    (int probabilityRangeAStart, int probabilityRangeAEnd, Enemy enemyA) = rangeA;
+                    (int probabilityRangeBStart, int probabilityRangeBEnd, Enemy enemyB) = rangeB;
+                    if (probabilityRangeBEnd < probabilityRangeAStart)
+                        return 1; // `rangeA` is "greater than" `rangeB`
+                    if (probabilityRangeBStart >= probabilityRangeAEnd)
+                        return -1; // `rangeA` is "less than" `rangeB`
+                    // The search point is either equal to the start of the checked range, or somewhere in the middle of it
+                    // (but not equal to the end of the range), and so the correct probability range has been found,
+                    // and the index of `rangeA` will be returned
+                    return 0; // `rangeA` is "equal" to `rangeB`
+                })
+        );
+        Debug.Log(foundIndex);
+        (int rangeStart, int rangeEnd, Enemy enemy) = spawnProbabilityRangesAndEnemies[foundIndex];
+        return enemy;
     }
 }

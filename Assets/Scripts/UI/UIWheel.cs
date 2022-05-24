097674b094a2aa845ff999929240f66562675967
Fredrik Eiding <fueredoriku@gmail.com>
2022-05-25 00:41:44 +0200

    Created new UI-wheel prefab, with new textures by @magsve. Created new script for uiwheel which should be way more modifiable in terms of adding new towers than the previous wheel logic when done.

---
+++ Assets/Scripts/UI/UIWheel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWheel : MonoBehaviour
+{
+
+    [SerializeField]
+    private TowerScriptableObject[] towers = default;
+    private List<Sprite> icons;
+
+    void Start()
+    {
+        foreach (TowerScriptableObject tower in towers)
+        {
+            icons.Add(tower.icon);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

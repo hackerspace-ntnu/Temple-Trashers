7b5ee4dcfbf5c243c31b13fd19110deb922420b4
Fredrik Eiding <fueredoriku@gmail.com>
2022-05-25 01:12:46 +0200

    Updated prefab to give relevant transforms to script. Started using said transforms in script to assign correct position and rotations to icons.

--- Assets/Scripts/UI/UIWheel.cs
+++ Assets/Scripts/UI/UIWheel.cs
@@ -7,13 +7,27 @@ public class UIWheel : MonoBehaviour
 
     [SerializeField]
     private TowerScriptableObject[] towers = default;
+    [SerializeField]
+    private Transform iconPositioner;
+    [SerializeField]
+    private Transform middleOrigin;
     private List<Sprite> icons;
+    private float iconDegree = default;
 
     void Start()
     {
+        //Add all available icon sprites to list
         foreach (TowerScriptableObject tower in towers)
         {
             icons.Add(tower.icon);
+        }
+        //Calculate what share of degress from the wheel each icon should have
+        iconDegree = 360/icons.Count;
+
+        for (int i = 0; i < icons.Count-1; i++)
+        {
+            //GameObject icon = Instantiate(icons[i], iconPositioner.position, transform.rotation, transform);
+            //icon.transform.RotateAround(middleOrigin.position, Vector3.forward, i*iconDegree);
         }
     }
 

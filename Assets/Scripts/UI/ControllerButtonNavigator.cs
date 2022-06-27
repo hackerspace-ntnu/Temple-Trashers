d9216071ea3ff1cb6f209fb709274610e1c604de
Fredrik Eiding <fueredoriku@gmail.com>
2022-06-27 23:56:08 +0200

    Added basic components needed to handle inputs for control over UI. The plan is to make buttons have a static self pointing property in which you can invoke the onclick delegate of Unity's UI system with the playerInput system.

---
+++ Assets/Scripts/UI/ControllerButtonNavigator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerButtonNavigator : MonoBehaviour
+{
+
+    public static ControllerButtonNavigator currentButton;
+    public ControllerButtonNavigator buttonUp;
+    public ControllerButtonNavigator buttonDown;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (!currentButton)
+        {
+            currentButton = this;
+        }
+    }
+
+    public void setCurrentButton()
+    {
+        //Reset highlight of previous currentButton
+        //currentButton.GetComponent<>();
+        currentButton = this;
+        //currentButton.GetComponent<>();
+    }
+
+    public void pressButton()
+    {
+        GetComponent<Button>().onClick.Invoke();
+    }
+
+}
---
+++ Assets/Scripts/UI/UIInputController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UIInputController : MonoBehaviour
+{
+    //private play
+    private PlayerInput playerInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        //playerInput.actions["Move"] += ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ControllerButtonNavigator.currentButton.buttonUp.setCurrentButton();
+    }
+}

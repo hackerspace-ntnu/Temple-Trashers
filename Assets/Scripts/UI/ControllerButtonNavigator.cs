e1a5d365ebccc3dd2505b9cd4ef7cc9091f7adee
Fredrik Eiding <fueredoriku@gmail.com>
2022-06-30 00:13:45 +0200

    Updated mainmenu scene buttons with the button navigator script. Added highlight function to buttons, though it still WIP as there are som bugs tied to which button actually gets highlighted.

--- Assets/Scripts/UI/ControllerButtonNavigator.cs
+++ Assets/Scripts/UI/ControllerButtonNavigator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class ControllerButtonNavigator : MonoBehaviour
@@ -9,13 +10,15 @@ public class ControllerButtonNavigator : MonoBehaviour
     public static ControllerButtonNavigator currentButton;
     public ControllerButtonNavigator buttonUp;
     public ControllerButtonNavigator buttonDown;
+    public ControllerButtonNavigator defaultButton;
 
     // Start is called before the first frame update
     private void Awake()
     {
         if (!currentButton)
         {
-            currentButton = this;
+            currentButton = defaultButton;
+            currentButton.GetComponent<Button>().Select();
         }
 
     }
@@ -23,8 +26,10 @@ public class ControllerButtonNavigator : MonoBehaviour
     public void SetCurrentButton()
     {
         //Reset highlight of previous currentButton
-        //currentButton.GetComponent<Button>();
+        //currentButton.GetComponent<Button>().
+        EventSystem.current.SetSelectedGameObject(null);
         currentButton = this;
+        currentButton.GetComponent<Button>().Select();
         //currentButton.GetComponent<>();
     }
 

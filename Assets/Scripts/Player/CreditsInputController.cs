f9c9bb3a84fba84a31e34a8d829a0867a0a26c0f
Dabble <ddabble@users.noreply.github.com>
2022-07-12 23:02:16 +0200

    Enabled skipping credits roll
    ...using the "Cancel_Menu" button (same as the "Cancel" button when playing the main game).
    
    Note: Input actions within different action maps can apparently not have the same name (hence "Cancel_Menu" instead of just "Cancel");
    otherwise, the action with the same name in the second action map will just be ignored.

---
+++ Assets/Scripts/Player/CreditsInputController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+
+public class CreditsInputController : MonoBehaviour
+{
+    private PlayerInput input;
+
+    private void CancelMenuInput_Performed(InputAction.CallbackContext ctx) => SceneManager.LoadScene("Main_Menu");
+
+    private void CancelMenuInput_Canceled(InputAction.CallbackContext ctx)
+    {}
+
+    void Awake()
+    {
+        input = GetComponent<PlayerInput>();
+
+        input.actions["Cancel_Menu"].performed += CancelMenuInput_Performed;
+        input.actions["Cancel_Menu"].canceled += CancelMenuInput_Canceled;
+    }
+
+    void OnDestroy()
+    {
+        input.actions["Cancel_Menu"].performed -= CancelMenuInput_Performed;
+        input.actions["Cancel_Menu"].canceled -= CancelMenuInput_Canceled;
+    }
+}

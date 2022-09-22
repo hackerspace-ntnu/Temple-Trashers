44edf72c3e0d67800f13272e3bc0845d7ffedf99
Thomas J. Gabrielsen <thomajga@stud.ntnu.no>
2022-09-22 13:32:41 +0200

    Difficulties!
    Added a new difficulty button.
    Difficulties are stored in a struct in MainMenu.cs.
    Difficulty is saved and loaded using playerprefs.

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,11 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+[Serializable]
+public struct Difficulty
+{
+    public string level;
+    public float spawnRate;
+    public float waveInterval;
+}
 
 public class MainMenu : MonoBehaviour
 {
+    public Difficulty[] difficulties;
+    private Difficulty selectedDifficulty;
+    public TextMeshProUGUI difficultyText;
+
+    private void Start()
+    {
+        selectedDifficulty = difficulties[0];
+        SaveDifficulty();
+    }
     // Simple function called to load the next scene from the built in unity button
     public void StartGame()
     {
@@ -30,5 +49,27 @@ public class MainMenu : MonoBehaviour
     public void Options()
     {
         SceneManager.LoadScene("Options");
+    }
+
+    public void ChangeDifficulty()
+    {
+        int index = Array.IndexOf(difficulties, selectedDifficulty);
+
+        if (index == difficulties.Length - 1)
+        {
+            selectedDifficulty = difficulties[0];
+        }
+        else
+            selectedDifficulty = difficulties[index + 1];
+        difficultyText.text = "Difficulty: " + selectedDifficulty.level;
+
+        SaveDifficulty();
+    }
+
+    private void SaveDifficulty()
+    {
+        PlayerPrefs.SetString("Difficulty", selectedDifficulty.level);
+        PlayerPrefs.SetFloat("spawnRate", selectedDifficulty.spawnRate);
+        PlayerPrefs.SetFloat("waveInterval", selectedDifficulty.waveInterval);
     }
 }

20396fdd15181e060cd9b4257a9f98ddd3ed8d7d
Fredrik Eiding <fueredoriku@gmail.com>
2023-03-12 23:45:44 +0100

    Added short intro scene. Updated ratio of gamepad texture.

---
+++ Assets/Scripts/UI/Intro.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform skipText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitForSceneChange());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator WaitForSceneChange()
+    {
+        yield return new WaitForSeconds(7f);
+        skipText.gameObject.SetActive(false);
+        yield return new WaitForSeconds(32f);
+        SceneManager.LoadScene("Main_Menu");   
+    }
+}
